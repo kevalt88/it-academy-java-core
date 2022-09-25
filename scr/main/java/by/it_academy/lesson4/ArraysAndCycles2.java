138f91f4d80a8cd2d0bb5ccb60fee3ab408c1d5b
keval <kevaltos1988@gmail.com>
2022-09-25 18:27:20 +0300

    Lesson4.ArraysAndCycles2. (Fixed with using cycles).

--- scr/main/java/by/it_academy/lesson4/ArraysAndCycles2.java
+++ scr/main/java/by/it_academy/lesson4/ArraysAndCycles2.java
@@ -2,15 +2,12 @@ package by.it_academy.lesson4;
 
 public class ArraysAndCycles2 {
     public static void main(String[] args) {
-        int[] array = new int[]{4, 6, 8};
-        for (int numberOfStars : array) {
-            if (numberOfStars == 4) {
-                System.out.println("****");
-            } else if (numberOfStars == 6)
-                System.out.println("******");
-            else if (numberOfStars == 8)
-                System.out.println("********");
+        int[] array = new int[]{10, 6, 8};
+        for (int i = 0; i < array.length; i++) {
+            for (int h = 0; h < array[i]; h++) {
+                System.out.print("*");
+            }
+            System.out.println();
         }
-
     }
 }
