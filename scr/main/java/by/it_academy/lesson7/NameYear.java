24492cd617c1f5575a502665061976fa302c157a
keval <kevaltos1988@gmail.com>
2022-10-09 18:40:17 +0300

    Lesson7.

---
+++ scr/main/java/by/it_academy/lesson7/NameYear.java
@@ -0,0 +1,19 @@
+package by.it_academy.lesson7;
+
+public class NameYear {
+    public static void main(String[] args) {
+        String[] array = new String[]{"Sasha,2000", "Stanislav,1995", "Roman,2015"};
+        double sum = 0;
+        String longestName = "";
+        for (String i : array) {
+            String[] nameYear = i.split(",");
+            String name = nameYear[0];
+            if (longestName.length() < name.length()) {
+                longestName = name;
+            }
+            sum += Integer.parseInt(nameYear[1]);
+        }
+        System.out.println(longestName);
+        System.out.println(sum / array.length);
+    }
+}
